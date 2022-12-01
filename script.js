569688bf2ef529e99ebf7d881844d67771c04bb6
balkissty <ogundotunbalkiss@gmail.com>
2022-12-01 12:45:50 +0100

    mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var show = document.getElementById("mobile-header");
+    if (show.style.display === "none") {
+      show.style.display = "block";
+    } else {
+      show.style.display = "none";
+    }
+  }
